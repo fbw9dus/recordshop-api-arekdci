e117be92d7d04c304f661aa5ef25f35ea259431b
arekdci <arekciura85@gmail.com>
2020-03-31 11:16:14 +0200

    test..

--- __tests__/endpoints-users.js
+++ __tests__/endpoints-users.js
@@ -9,6 +9,12 @@ let server;
 
 describe('Users Endpoints', () => {
     test('should get list of all users', async done =>{
+        await User.create({
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            email: faker.internet.email(),
+            password: faker.internet.password()
+        })
         const res = await request(app).get('/users')
         expect(res.statusCode).toBe(200)
         expect(Array.isArray(res.body)).toBeTruthy()
@@ -75,14 +81,13 @@ describe('Users Endpoints', () => {
         const res = await request(app)
             .post(`/users`)
             .send(fakeUser)
-        const checkUser = await User.findOne({'email': fakeUser.email})
+        const checkUser = await User.findById(res.body._id)
         expect(checkUser).toHaveProperty(['email'])
         done()
     })
 })
 
 beforeAll(async (done) => {
-    exec('npm run seed')
     server = app.listen(3000, () => {
         global.agent = request.agent(server);
         done();
